bbe9b5c3b9fb3949ccd8d8d674654620a6ebbec8
murtajizdevwork-tech <murtajizdevwork@gmail.com>
2026-07-24 22:18:19 +0500

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,12 +1,12 @@
 import path from "path";
 import fs from "fs";
-import express, { type Express } from "express";
+import express from "express";
 import cors from "cors";
 import { pinoHttp } from "pino-http";
 import router from "./routes/index.js";
 import { logger } from "./lib/logger.js";
 
-const app: Express = express();
+const app = express();
 
 app.use(
   pinoHttp({
